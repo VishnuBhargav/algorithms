9a774de6c95f24325eb28cd0baa3a7b1a8cd0d87
vichu259 <vichu259@gmail.com>
2018-07-29 16:08:55 +0530

    atom test

--- data-structures/binary-tree/node.py
+++ data-structures/binary-tree/node.py
@@ -1,10 +1,18 @@
 #!/bin/python
 
+
 class node:
-    def __init__(self,value):
-        self.parent=None;
-        self.left=None;
-        self.right=Nosad dfne;
-        self.value=value;
-        
+    def __init__(self, value):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.value = value
+
+
 def nodeTest():
+    node1 = node(1)
+    print(node1.value)
+
+
+if (__name__ == '__main__'):
+    nodeTest()
